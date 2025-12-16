99826fe6e990855b23991ddc6ee600ee1de972f4
LLMirror <veloster@126.com>
2025-12-16 10:37:03 +0800

    refactor(router): 移除注释的路由配置并调整表格列对齐方式
    将/yh-zjmxjsc和/yh-biaoqian路由配置改为注释状态
    统一表格列内容居中显示，并恢复显示ID列

--- item/src/views/yh-biaoqian/index.vue
+++ item/src/views/yh-biaoqian/index.vue
@@ -28,12 +28,12 @@
 
     <el-card class="table-card" shadow="never">
       <el-table :data="list" border style="width: 100%">
-        <!-- <el-table-column prop="id" label="ID" width="90" /> -->
-        <el-table-column prop="rolesId" label="公司" width="120" />
-        <el-table-column prop="parent" label="大类" />
-        <el-table-column prop="child" label="子类" />
-        <el-table-column prop="remark" label="备注" />
-        <el-table-column label="操作" width="180">
+        <el-table-column prop="id" label="ID" width="90" align="center" />
+        <el-table-column prop="rolesId" label="公司" width="120" align="center" />
+        <el-table-column prop="parent" label="大类" align="center" />
+        <el-table-column prop="child" label="子类" align="center" />
+        <el-table-column prop="remark" label="备注" align="center" />
+        <el-table-column label="操作" width="180" align="center">
           <template #default="{ row }">
             <el-button type="primary" link @click="openEdit(row)">编辑</el-button>
             <el-button type="danger" link @click="handleDelete(row)">删除</el-button>
